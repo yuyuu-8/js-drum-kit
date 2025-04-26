b590f09ded6d41f0600682cb4ebabdfdfbb53fd0
yuyu <sishichulu@gmail.com>
2025-04-26 23:34:53 +0300

    feat: add transition and info button

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-window.addEventListener("keydown", (e) => {
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!audio) return;
+
+  key.classList.add("playing");
   audio.currentTime = 0;
   audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
+
+const modal = document.querySelector(".modal");
+const info = document.querySelector("#info");
+const close = document.querySelector("#close");
+info.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+close.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
 });
